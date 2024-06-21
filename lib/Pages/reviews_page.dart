b4e4d06c4312becfcab2f0e78b92beb6cb08f396
Sukhi Singh <44445119+SukhiM@users.noreply.github.com>
2024-06-21 09:35:31 -0400

    First

---
+++ lib/Pages/reviews_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReviewsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reviews Page'),
+      ),
+      body: Center(
+        child: Text('Reviews Page'),
+      ),
+    );
+  }
+}
